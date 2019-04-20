9c51a9fceabc20c6e3425f00df0270e8ccccc532
aeshes <mathesis@inbox.ru>
2019-04-20 16:45:21 +0200

    Parse current module's import table

--- Kernel32_Base/Kernel32Base.asm
+++ Kernel32_Base/Kernel32Base.asm
@@ -60,4 +60,51 @@ NextPage:
     xor eax, eax
     ret
 GetBase endp
+
+GetKernelImport proc
+    push esi
+    push ebx
+    push edi
+    call AddrInModule
+AddrInModule:
+    mov esi, dword ptr[esp] ; Получаем адрес внутри текущего модуля
+    add esp, 4
+    and esi, 0FFFF0000h     ; Используем гранулярность памяти
+ModuleBegin:
+    call ValidPE
+    .if eax == 0
+        sub esi, 10000h
+        jmp ModuleBegin
+    .endif
+    mov ebx, esi            ; Сохраняем базу текущего модуля
+    assume esi: ptr IMAGE_DOS_HEADER
+    add esi, [esi].e_lfanew ; Находим PE-заголовок
+
+    assume esi: ptr IMAGE_NT_HEADERS
+    lea esi, [esi].OptionalHeader
+
+    assume esi: ptr IMAGE_OPTIONAL_HEADER
+    lea esi, [esi].DataDirectory
+    add esi, 8              ; К элементу массива с индексом 1
+    mov eax, ebx
+    add eax, dword ptr [esi]
+    mov esi, eax            ; В esi - смещение таблицы импорта
+    assume esi: ptr IMAGE_IMPORT_DESCRIPTOR
+    mov edi, [esi].Name1
+    add edi, ebx
+    .if dword ptr [edi] == "nrek"
+        mov edi, [esi].FirstThunk
+        add edi, ebx
+        mov eax, dword ptr [edi]
+        pop edi
+        pop ebx
+        pop esi
+        ret
+    .endif
+    add esi, sizeof IMAGE_IMPORT_DESCRIPTOR
+    jmp NextDLL
+NextDLL:
+    
+GetKernelImport endp
+
 end start
